78a295fe6e6377bbc5b86d44eb16aea4d8ccd0f4
kciejek <konrad.ciejek+github@gmail.com>
2020-10-28 13:42:36 +0100

    Random move moves

--- M.java
+++ M.java
@@ -1,15 +1,46 @@
+package com.work.simulator;
+
+import static java.util.concurrent.ThreadLocalRandom.current;
+
 import java.awt.AWTException;
+import java.awt.Dimension;
 import java.awt.Robot;
+import java.awt.Toolkit;
 
 public class M {
 
   public static void main(String[] args) throws AWTException {
     final Robot robot = new Robot();
     final int SLEEP_TIME = 10000;//in ms
-    while(true){
-      robot.mouseMove(0, 0);
+    // java - get screen size using the Toolkit class
+    final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    // the screen height
+    final int y = (int) screenSize.getHeight();
+    // the screen width
+    final int x = (int) screenSize.getWidth();
+    final RandomValue randomValue = new RandomValue(x, y);
+    while (true) {
+      robot.mouseMove(randomValue.getBoundX(), randomValue.getBoundY());
       robot.delay(SLEEP_TIME);
-      robot.mouseMove(1, 0);
+    }
+  }
+
+  private static class RandomValue {
+
+    private int boundX;
+    private int boundY;
+
+    public RandomValue(int boundX, int boundY) {
+      this.boundX = boundX;
+      this.boundY = boundY;
+    }
+
+    public int getBoundX() {
+      return current().nextInt(0, boundX);
+    }
+
+    public int getBoundY() {
+      return current().nextInt(0, boundY);
     }
   }
 }
